dc9de1c3e9e2ef04e9efe953793176e94660489a
Naresh Jain <nashjain@gmail.com>
2025-04-29 20:21:05 +0530

    using yaml dictionary instead of json dictionary

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ docker run \
       -v "$PWD/openapi.yaml:/usr/src/app/openapi.yaml" \
       -v "$PWD/specmatic.yaml:/usr/src/app/specmatic.yaml" \
       -v "$PWD/openapi_examples:/usr/src/app/openapi_examples" \
-      -v "$PWD/openapi_dictionary.json:/usr/src/app/openapi_dictionary.json" \
+      -v "$PWD/openapi_dictionary.yaml:/usr/src/app/openapi_dictionary.yaml" \
       -e SPECMATIC_LICENSE_PATH=/usr/src/app/specmatic-license.txt \
       znsio/specmatic-openapi examples interactive --spec-file openapi.yaml
